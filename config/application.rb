c1bcafef1942a9d9064e6ea42645ed407905cc97
teddymeksavanh <ventialaw@hotmail.fr>
2018-07-18 05:01:10 +0200

    heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Gow4lkRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.read_encrypted_secrets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
